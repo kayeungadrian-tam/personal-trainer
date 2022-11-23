9d55c40fc3163a1a5747f5e0b9a3d35c9e13b49e
adrian <tamkayeung.adrian@gmail.com>
2022-11-24 01:14:51 +0900

    add camera devices

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="hello">
-    <h1>TESING</h1>
-    {{ devicesList }}
+    <h1>{{ angle }}</h1>
+    <h1>{{ counter }}</h1>
+    <h1>{{ stage }}</h1>
+    <select v-model="videoDivice"
+      :items="devicesList">
+      <option v-for="device in devicesList"
+        :key=device>
+        {{ device }}
+      </option>
 
+    </select>
+
+    {{ videoDivice }}
     <div class="webcam">
       <video class="web_video"
         ref="webVideo"
@@ -17,8 +27,8 @@
 
 <script lang="ts">
 import { Camera } from '@mediapipe/camera_utils';
-import { Holistic, Results, POSE_CONNECTIONS, FACEMESH_TESSELATION, HAND_CONNECTIONS } from '@mediapipe/holistic';
-import { ref, onMounted, defineComponent } from "vue";
+import { Holistic, Results, POSE_CONNECTIONS, FACEMESH_TESSELATION, HAND_CONNECTIONS, NormalizedLandmark } from '@mediapipe/holistic';
+import { ref, onBeforeUnmount, watch, onMounted, defineComponent } from "vue";
 import type { InputImage } from "@mediapipe/holistic";
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 const UNSELECTED = "empty";
@@ -32,7 +42,30 @@ export default defineComponent({
 </script>
 
 <script setup lang="ts">
+const videoDivice = ref(localStorage.getItem('videoDivice') || '')
+const counter = ref(0);
+const angle = ref(0);
+const stage = ref("hello");
 
+const finaAngle = (p0: NormalizedLandmark, p1: NormalizedLandmark, p2: NormalizedLandmark) => {
+  const a = Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2);
+  const b = Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+  const c = Math.pow(p2.x - p0.x, 2) + Math.pow(p2.y - p0.y, 2);
+  const radians = Math.acos((a + b - c) / Math.sqrt(4 * a * b));
+  return radians * (180 / Math.PI);
+};
+
+const countPushUps = (angle: number) => {
+  if (angle > 45) {
+    stage.value = "up"
+  } else if (angle <= 20) {
+    if (stage.value === "up") {
+      stage.value = "down"
+      counter.value += 1
+    }
+  }
+
+}
 
 
 const webVideo = ref<HTMLVideoElement>()
@@ -90,7 +123,17 @@ const onResults = (results: Results) => {
 
 const detectHolistic = (results: Results) => {
   if (results.poseLandmarks) {
-    console.log("detected");
+    // console.log(results.poseLandmarks[11].x);
+    // const x = results.poseLandmarks[386].x;
+    const p0 = results.poseLandmarks[23];
+    const p1 = results.poseLandmarks[11];
+    const p2 = results.poseLandmarks[13];
+
+    const tmp = finaAngle(p0, p1, p2);
+    // console.log(tmp);
+    angle.value = tmp;
+    countPushUps(tmp);
+
     drawConnectors(ctx.value, results.poseLandmarks, POSE_CONNECTIONS, {
     });
     drawLandmarks(ctx.value, results.poseLandmarks,
@@ -111,6 +154,19 @@ const detectHolistic = (results: Results) => {
   }
 }
 
+const stopCamera = () => {
+  if (!webVideo.value?.srcObject) {
+    return
+  }
+  const stream = webVideo.value.srcObject as MediaStream
+  const tracks = stream.getTracks()
+  tracks.forEach((track) => {
+    track.stop()
+  })
+  webVideo.value.srcObject = null
+  ctx.value.clearRect(0, 0, 1280, 720)
+  camera.value?.stop()
+}
 
 
 onMounted(() => {
@@ -118,13 +174,28 @@ onMounted(() => {
   ctx.value = outVideo?.value?.getContext('2d')
 })
 
+watch(
+  () => videoDivice.value,
+  () => {
+    localStorage.setItem('videoDivice', videoDivice.value || '')
+    if (videoDivice.value !== UNSELECTED) {
+      startCamera()
+    }
+    else {
+      stopCamera();
+      console.log("No camera selected")
+    }
+  }
+)
 
+onBeforeUnmount(async () => {
+  stopCamera()
+})
 
 const devicesList = ref<string[]>([UNSELECTED]); // videoリスト
 navigator.mediaDevices
   .getUserMedia({ audio: false, video: true })
   .then((stream) => {
-    // 入出力デバイスの取得
     navigator.mediaDevices
       .enumerateDevices()
       .then((devices) => {
