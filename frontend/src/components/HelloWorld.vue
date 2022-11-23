3363594e67b5255600db4ed754487675aac9f3e1
adrian <tamkayeung.adrian@gmail.com>
2022-11-24 00:24:00 +0900

    🚀 add VUE

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="hello">
+    <h1>TESING</h1>
+    {{ devicesList }}
+
+    <div class="webcam">
+      <video class="web_video"
+        ref="webVideo"
+        hidden></video>
+      <canvas ref="outVideo"
+        class="outVideo"
+        width=1280
+        height=720></canvas>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Camera } from '@mediapipe/camera_utils';
+import { Holistic, Results, POSE_CONNECTIONS, FACEMESH_TESSELATION, HAND_CONNECTIONS } from '@mediapipe/holistic';
+import { ref, onMounted, defineComponent } from "vue";
+import type { InputImage } from "@mediapipe/holistic";
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+const UNSELECTED = "empty";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<script setup lang="ts">
+
+
+
+const webVideo = ref<HTMLVideoElement>()
+const outVideo = ref<HTMLCanvasElement>()
+
+
+
+const camera = ref<Camera>()
+
+
+const startCamera = () => {
+  const holistic = new Holistic({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+    }
+  });
+  holistic.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: true,
+    smoothSegmentation: true,
+    refineFaceLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+
+  holistic.onResults(onResults)
+
+  if (webVideo.value) {
+    camera.value = new Camera(webVideo.value, {
+      onFrame: async () => {
+        await holistic.send({ image: webVideo.value as InputImage });
+      },
+      width: 1280,
+      height: 720
+    });
+    camera.value.start();
+  }
+}
+
+
+const ctx = ref()
+
+const onResults = (results: Results) => {
+  ctx.value.save()
+  ctx.value.clearRect(0, 0, results.image.width, results.image.height)
+  ctx.value.drawImage(results.image, 0, 0, results.image.width, results.image.height)
+  // cameraのサイズをストアに保存する
+  detectHolistic(results)
+
+
+  ctx.value.restore()
+}
+
+
+const detectHolistic = (results: Results) => {
+  if (results.poseLandmarks) {
+    console.log("detected");
+    drawConnectors(ctx.value, results.poseLandmarks, POSE_CONNECTIONS, {
+    });
+    drawLandmarks(ctx.value, results.poseLandmarks,
+      { color: '#FCAE1E', lineWidth: 2 });
+    drawConnectors(ctx.value, results.faceLandmarks, FACEMESH_TESSELATION,
+      { color: '#C0C0C070', lineWidth: 1 });
+    drawConnectors(ctx.value, results.leftHandLandmarks, HAND_CONNECTIONS,
+      { color: '#00FF00', lineWidth: 5 });
+    drawLandmarks(ctx.value, results.leftHandLandmarks,
+      { color: '#00FF00', lineWidth: 2 });
+    drawConnectors(ctx.value, results.rightHandLandmarks, HAND_CONNECTIONS,
+      { color: '#CC0000', lineWidth: 5 });
+    drawLandmarks(ctx.value, results.rightHandLandmarks,
+      { color: '#CC0000', lineWidth: 2 });
+  }
+  else {
+    console.log("Not detected.")
+  }
+}
+
+
+
+onMounted(() => {
+  startCamera();
+  ctx.value = outVideo?.value?.getContext('2d')
+})
+
+
+
+const devicesList = ref<string[]>([UNSELECTED]); // videoリスト
+navigator.mediaDevices
+  .getUserMedia({ audio: false, video: true })
+  .then((stream) => {
+    // 入出力デバイスの取得
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) => {
+        devices.forEach((device) => {
+          if (device.kind === "videoinput")
+            devicesList.value.push(device.label);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
